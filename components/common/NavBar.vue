0778ab5eb5224daa3c1e54256dcef5017242143a
Dakeem05 <edidiongsamuel14@gmail.com>
2024-04-18 13:51:31 +0100

    Wallet connect confirmation

--- components/common/NavBar.vue
+++ components/common/NavBar.vue
@@ -10,7 +10,8 @@ const projectId = '064d0a124217d401cda6999baf496215'
 const metadata = {
   name: 'Compad | Private Presale',
   description: 'Compad private presale website',
-  url: 'http://localhost:3000/', // origin must match your domain & subdomain
+  url: 'https://compad-presale.vercel.app/', // origin must match your domain & subdomain
+  // url: 'http://localhost:3000/', // origin must match your domain & subdomain
   icons: ['https://pbs.twimg.com/profile_images/1685353964450004993/7q2iSxcW_400x400.jpg']
 }
 
