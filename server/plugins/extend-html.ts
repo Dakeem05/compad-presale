aa9c680896b5025e4686cba9a6050f8089dd9f68
Dakeem05 <edidiongsamuel14@gmail.com>
2024-04-18 05:28:45 +0100

    Worked on SEO

--- server/plugins/extend-html.ts
+++ server/plugins/extend-html.ts
@@ -6,10 +6,10 @@ export default defineNitroPlugin((nitroApp) => {
       html.head.push(`<link rel="icon" type="image/x-icon" href="images/logo.png" />`);
       html.head.push(`<title>Compad | Private Presale</title>`);
       html.head.push(`<meta property="og:type" content="website" />`);
-      html.head.push(`<meta property="og:url" content="https://edidiong-samuel.vercel.app/" />`);
+      html.head.push(`<meta property="og:url" content="https://compad-presale.vercel.app/" />`);
       html.head.push(`<meta property="og:title" content="Compad | Private Presale" />`);
       html.head.push(`<meta property="og:description" content="Compad private presale website" />`);
-      html.head.push(`<meta property="og:image" content="images/logo.png" />`);
+      html.head.push(`<meta property="og:image" content="https://pbs.twimg.com/profile_images/1685353964450004993/7q2iSxcW_400x400.jpg" />`);
       html.head.push(`<meta name="twitter:card" content="Compad private presale website" />`);
       html.head.push(`<meta name="twitter:url" content="https://x.com/Compadofficial?t=O7E36yY3IgiypnzQ9f9MwQ&s=09" />`);
       html.head.push(`<meta name="twitter:title" content="Compad | Private Presale" />`);
